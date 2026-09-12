108265b7c10bfe6ae8008ae8dbc1542bead804be
hafuyy <hafidfurqon07@gmail.com>
2026-09-12 19:35:35 +0700

    Feat: Penambahan program connect firebase dan wifi untuk MK

--- Intensive-IOT-kel1/Intensive-IOT-kel1.ino
+++ Intensive-IOT-kel1/Intensive-IOT-kel1.ino
@@ -1,3 +1,32 @@
+#include <WiFi.h>
+#include <Firebase_ESP_Client.h>
+#include <addons/TokenHelper.h>
+// hafed ganteng banget kk
+
+//======================================================
+// WIFI
+//======================================================
+
+#define WIFI_SSID "hafeddd"
+#define WIFI_PASSWORD "12345678"
+
+//======================================================
+// FIREBASE
+//======================================================
+
+#define API_KEY "AIzaSyBbPPclDmwY3Q1ym_btxYgF7Ps8Q71M41A"
+#define DATABASE_URL "https://tes-firebase-7a848-default-rtdb.firebaseio.com/"
+
+//======================================================
+// FIREBASE OBJECT
+//======================================================
+
+FirebaseData fbdo;
+FirebaseAuth auth;
+FirebaseConfig config;
+
+bool signupOK = false;
+
 void setup() {
   // put your setup code here, to run once:
 // mas hafid tampan
